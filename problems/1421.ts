b9bec6b465e573fc867e277c16ba3b23ab7a7cb7
Michael Richins <mr.grigri@gmail.com>
2025-11-03 20:21:39 -0700

    Remove: Console log

--- problems/1421.ts
+++ problems/1421.ts
@@ -11,6 +11,6 @@ function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
   return output;
 }
 
-equals(kidsWithCandies([2, 3, 5, 1, 3], 3), [true, true, true, false, true]); //?
-equals(kidsWithCandies([4, 2, 1, 1, 2], 1), [true, false, false, false, false]); //?
-equals(kidsWithCandies([12, 1, 12], 10), [true, false, true]); //?
+equals(kidsWithCandies([2, 3, 5, 1, 3], 3), [true, true, true, false, true]);
+equals(kidsWithCandies([4, 2, 1, 1, 2], 1), [true, false, false, false, false]);
+equals(kidsWithCandies([12, 1, 12], 10), [true, false, true]);
